d6da1c6c415731cf8a70b515989136e9c95416cf
lukmanul6305 <lukmanulhakim6305@gmail.com>
2025-04-18 22:21:28 +0700

    update bisa gk

--- src/Main.js
+++ src/Main.js
@@ -5,12 +5,26 @@ class Main extends Component {
     super(props);
     this.state = {
       title: "Menu Makanan",
+      title2: "Menu minuman",
+      inputValue: "lukman",
     };
 
     this.rubahData = this.rubahData.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
   rubahData() {
-    this.setState({ title: "pilih makanan" });
+    this.setState(() => {
+      return {
+        title: this.state.title2,
+        title2: this.state.title,
+      };
+    });
+  }
+
+  handleChange(e) {
+    this.setState({
+      inputValue: e.target.value,
+    });
   }
 
   render() {
@@ -18,6 +32,9 @@ class Main extends Component {
       <>
         <h3>{this.state.title}</h3>
         <button onClick={this.rubahData}>ubah Data</button>
+        <br />
+        <input type="text" value={this.setState.inputValue} onChange={this.handleChange} />
+        <p>isi input : {this.state.inputValue}</p>
       </>
     );
   }
